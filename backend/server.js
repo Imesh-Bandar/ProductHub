80237c5305fe2ab5a64ac79d43308873e442c77e
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-06-10 17:24:00 +0530

    create simple router

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+
+//create a simple server
+app.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON PORT${PORT}`);
+});
