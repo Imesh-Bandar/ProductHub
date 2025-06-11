904a071dd561064463e14c9fb3f7a143be3b662d
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-06-11 16:20:37 +0530

    minor changers added

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,14 @@
 import express from 'express';
-
-
-
 const app = express();
+
+
+
+
+
 const PORT = process.env.PORT || 3000;
-
-
-
-
 app.get('/products', (req, res) => {
     res.send('Hello, World!');
 });
-
-
 //create a simple server
 app.listen(PORT, () => {
     console.log(`SERVER IS RUNNING ON PORT http://localhost:${PORT}`);
