747c09023d0b4123c8be0ca1395754492eb7ba95
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-06-18 21:03:56 +0530

    create simple route

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,13 @@ import dotenv from "dotenv";
 const app= express();
 const port =3000;
 
+
+//create simple get route
+app.get("/",(req,res)=>{
+    res.send("GET Method is ready to use");
+})
+
+
 //create simple server
 app.listen(port,()=>{
     console.log("Server started on port "+port);
