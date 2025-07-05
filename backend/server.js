67a3b0f223591ecfe6ea13ef2e5ae5b34c5c71b7
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-05 13:41:54 +0530

    refactor server.js to improve structure and add MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectMongoDB } from './config/dbconnection.js';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = 5000;
+
+// implement the root route
+app.get('/', (req, res) => {
+    res.send('Welcome to ProductHub API');
+})
 
 
-const app =express();
-const PORT =5000;
+// //LOG ENVIRONMENT VARIABLES
+// console.log('Environment Variables:');
+// console.log(process.env.MONGO_URL);
 
 
+// create simple server
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    connectMongoDB();
+    console.log(`Server started successfully at http://localhost:${PORT}`);
 });
