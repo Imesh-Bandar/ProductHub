506685ac8779bd3ac0eb65704322753dc48616a6
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-05 13:41:19 +0530

    add MongoDB connection setup

---
+++ backend/config/dbconnection.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+
+export const connectMongoDB = async () => {
+    try {
+        //Load environment variables from .env file
+        const MONGO_URL = process.env.MONGO_URL;
+        //create a connection with MongoDB
+        const conn = await mongoose.connect(MONGO_URL);
+
+
+        //check if the connection is successfull
+        if (conn) {
+            console.log("MongoDB connected successfully");
+            console.log("hostname",conn.connection.host);
+        }
+
+    } catch (error) {
+        console.log("Cannot Connected to the mongodb database", error.message);
+        process.exit(1); //1 means failure,0 means success
+    }
+
+}
