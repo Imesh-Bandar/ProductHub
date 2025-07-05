0c36d4051ff9eb5feed722d51ad8db90b8aa86e6
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-05 15:56:53 +0530

    implement product fetching route with error handling and improve response structure

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,31 @@ dotenv.config();
 app.use(express.json());
 
 
+
+
+
+app.get('/api/product/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log("Parameter ID:", id);
+
+    try {
+        const product = await Product.findById(id);
+        // Check if product exists
+        if (!product) {
+            return res.status(404).json({ status: false, message: "Product not found." });
+
+        } else {
+            // If product exists, send it in the response
+            res.status(200).json({ status: true, productDetails: product });
+        }
+    }catch (error) {
+        console.error("Error fetching product:", error.message);
+        res.status(500).json({ status: false, message: "Internal server error while fetching product", });
+    }
+
+})
+
+
 //==================|| Product API Routes START -UPDATE ||==================//
 
 app.put('/api/product/:id', async (req, res) => {
@@ -36,12 +61,12 @@ app.put('/api/product/:id', async (req, res) => {
             const updateProduct = await Product.findByIdAndUpdate(id, updatedProductData, { new: true });
             //send the response to the frontend
 
-            res.status(200).json({status: true,message: "Product updated successfully", productDetails: updateProduct});
+            res.status(200).json({ status: true, message: "Product updated successfully", productDetails: updateProduct });
         }
 
     } catch (error) {
         console.error("Error updating product:", error.message);
-        res.status(500).json({ status: false, message: "Internal server error while updating product",});
+        res.status(500).json({ status: false, message: "Internal server error while updating product", });
 
     }
 
@@ -76,10 +101,10 @@ app.get('/api/product', async (req, res) => {
         // Fetch all products from the database
         const products = await Product.find({});
         // If no products found, return an empty array
-        res.status(200).json({ status: true, products: products});
+        res.status(200).json({ status: true, products: products });
     } catch (error) {
         console.error("Error fetching products:", error.message);
-        res.status(500).json({status: false,  message: "Internal server error while fetching products", });
+        res.status(500).json({ status: false, message: "Internal server error while fetching products", });
     }
 });
 
@@ -109,23 +134,23 @@ app.post("/api/product", async (req, res) => {
     if (!product.name || !product.price || !product.des || !product.imge) {
 
         // If product is not valid, return an error response
-        return res.status(400).json({status: false,message:  "Please provide all required fields: name, price, description, and image URL.",});
+        return res.status(400).json({ status: false, message: "Please provide all required fields: name, price, description, and image URL.", });
 
     } else {
         // If product is valid, save it to the database
         try {
             //Import the product model and create a new product instance using frontend customer send data
-            const newProduct = new Product({name: product.name, price: product.price,des: product.des,imge: product.imge,});
+            const newProduct = new Product({ name: product.name, price: product.price, des: product.des, imge: product.imge, });
             // Save the product to the mongodb database
             await newProduct.save();
 
             //send the response to the frontend
-            res.status(201).json({status: true,message: "Product created successfully",productDetails: newProduct,});
+            res.status(201).json({ status: true, message: "Product created successfully", productDetails: newProduct, });
 
             //if the error occursing during the saving of product in database
         } catch (error) {
             console.error("Error creating product:", error.message);
-            res.status(500).json({ status: false, message: "Internal server error while creating product",});
+            res.status(500).json({ status: false, message: "Internal server error while creating product", });
         }
     }
 });
@@ -151,7 +176,7 @@ app.delete("/api/product/:id", async (req, res) => {
         try {
             await Product.findByIdAndDelete(id);
             //send the response to the frontend
-            res.status(200).json({status: true, message: "Product deleted successfully" });
+            res.status(200).json({ status: true, message: "Product deleted successfully" });
         } catch (error) {
             res.status(404).json({ status: true, message: "Product not found or could not be deleted", });
         }
