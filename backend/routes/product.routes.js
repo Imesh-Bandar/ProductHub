117fa79bc468230a676259e19ad8fa45ae4b66e4
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-05 16:33:58 +0530

    refactor: clean up database connection and product routes for better readability

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,47 +1,20 @@
-import express, { application } from "express";
-
+import express from "express";
+// Import the product controller functions
 import { createProduct, deleteProduct, editProduct, getProduct, getProducts } from "../controller/product.controller.js";
 
-const app = express();
-const router = express.Router();
-// Middleware to parse JSON data from request body
 
 // base route for product API
 //app.use("/api/product", router);
+const router = express.Router();
+// Define the routes for product operations
 
+router.get('/', getProducts);// Get all products
+router.get('/:id', getProduct)// Get a single product by ID
+router.post("/create", createProduct);// Create a new product
+router.put('/edit/:id', editProduct)// Edit a product by ID
+router.delete("/delete/:id", deleteProduct);// Delete a product by ID
 
-
-//==================|| Product API Routes START -POST ||==================//
-//Implement the product create route
-router.post("/create", createProduct);
-//==================|| Product API Routes END -POST ||==================//
-
-
-
-//==================|| Product API Routes START -GET BY ID ||==================//
-router.get('/:id', getProduct)
-//==================|| Product API Routes END -GET BY ID ||==================//
-
-
-
-
-
-//==================|| Product API Routes START -GET ||==================//
-// This route will handle the fetching of all products
-router.get('/', getProducts);
-//==================|| Product API Routes END -GET ||==================//
-
-
-
-//==================|| Product API Routes START -UPDATE ||==================//
-router.put('/edit/:id', editProduct)
-//==================|| Product API Routes END -UPDATE ||==================//
-
-
-//==================|| Product API Routes START - DELETE ||==================//
-router.delete("/delete/:id", deleteProduct);
-
-//==================|| Product API Routes END - DELETE ||==================//
+ 
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectMongoDB } from "./config/dbconnection.js";
-// Import the product model
+// Import the product routes
 import productRoutes from "./routes/product.routes.js";
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
+
 // Load environment variables from .env file
 dotenv.config();
+
+
 // Middleware : allow us to parse JSON data from the request body
 app.use(express.json());
-//call the product routes
-app.use("/api/product", productRoutes);
-
-
+app.use("/api/product", productRoutes);// Define the base route for product API
 
 //==================|| Connect to MongoDB and start the server ||==================//
 
