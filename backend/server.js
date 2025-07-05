677a52fe3c735e9cb52ed874e95fb154e0599523
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-05 16:45:06 +0530

    fix: update PORT in .env and server.js for consistency

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,27 @@ import { connectMongoDB } from "./config/dbconnection.js";
 // Import the product routes
 import productRoutes from "./routes/product.routes.js";
 
+
+
+// ==================|| Server Setup ||================== //
 const app = express();
-const PORT = 5000 || process.env.PORT;
-
-
+// Middleware : allow us to parse JSON data from the request body
+app.use(express.json());
 // Load environment variables from .env file
 dotenv.config();
 
+const PORT =  process.env.PORT || 3000; // Set the port from environment variable or default to 3000;
 
-// Middleware : allow us to parse JSON data from the request body
-app.use(express.json());
+
+
+
+//==================|| Routes Setup ||================== //
 app.use("/api/product", productRoutes);// Define the base route for product API
 
-//==================|| Connect to MongoDB and start the server ||==================//
 
+
+
+//==================|| Connect to MongoDB and start the server ||==================//
 // Start the server and connect to MongoDB
 app.listen(PORT, () => {
     // Connect to MongoDB
